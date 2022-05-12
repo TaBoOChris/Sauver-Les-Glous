59ba79a68524e335b16e4e040c875e447501ca68
CavaniNicolas <cavani.nicolas6@gmail.com>
2022-05-12 16:25:17 -0400

    [dev] add MainMenu Scene, Button prefab with animator

---
+++ Unity Sauver Les Glous/Assets/Scripts/Menu/Button.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log("Load Scene " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
