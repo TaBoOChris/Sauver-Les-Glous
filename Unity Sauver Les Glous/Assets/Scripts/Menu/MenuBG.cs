e7a4c88b9e4e9c1fd387149b845ef52c477ba4a7
THIBAUD SIMON <tsimon1@etu.uqac.ca>
2022-05-23 15:00:41 -0400

    dynamic menu design

---
+++ Unity Sauver Les Glous/Assets/Scripts/Menu/MenuBG.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBG : MonoBehaviour
+{
+    [SerializeField] Transform spawner;
+    [SerializeField] GameObject glou;
+    [SerializeField] float spawnInterval = 2.0f;
+    [SerializeField] float xSpawnForce = 180;
+    [SerializeField] float ySpawnForce = 180;
+    float t = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = spawnInterval - 0.5f;
+    }
+
+    void SpawnGlou()
+    {
+        GameObject newGlou = Instantiate(glou, spawner.position, Quaternion.identity);     // Spawn Glou
+        //AudioManager.Instance.PlayGlouSpawn();                                          // Glou spawn sound
+        newGlou.transform.parent = this.transform;
+        newGlou.GetComponentInChildren<Rigidbody2D>().AddForce(new Vector2(xSpawnForce+Random.Range(-50f,50f), -ySpawnForce)); // Add force on the new glou
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t+=Time.deltaTime;
+        if(t > spawnInterval)
+        {
+            SpawnGlou();
+            t = 0;
+        }
+    }
+}
