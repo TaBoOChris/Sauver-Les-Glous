55bc78b030530a1de752e0f78b3f332a86db41b1
CavaniNicolas <cavani.nicolas6@gmail.com>
2022-05-29 17:30:27 -0400

    [dev] rearrange code style

--- Unity Sauver Les Glous/Assets/Scripts/Gameplay/GlousSpawner.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Gameplay/GlousSpawner.cs
@@ -4,39 +4,35 @@ using UnityEngine;
 
 public class GlousSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject glou;
-    [SerializeField] Transform spawnTransform;
-    [SerializeField] float spawnDelay = 1f;
-    [SerializeField] float xMaxSpawnForce = 50;
-    [SerializeField] float ySpawnForce = 180;
-    [SerializeField] float scaleMax = 1.2f;
-    [SerializeField] float scaleMin = 0.6f;
-
-
-
+    [SerializeField] private GameObject m_glou;
+    [SerializeField] private Transform m_spawnTransform;
+    [SerializeField] private float m_spawnDelay = 1f;
+    [SerializeField] private float m_xMaxSpawnForce = 50;
+    [SerializeField] private float m_ySpawnForce = 180;
+    [SerializeField] private float m_scaleMax = 1.2f;
+    [SerializeField] private float m_scaleMin = 0.6f;
 
     public void SpawnGlous(int glousNumber){
         StartCoroutine(SpawnGlousCoroutine(glousNumber));
     }
 
-
     IEnumerator SpawnGlousCoroutine(int glousNumber)
     {
-        for (int i =0; i < glousNumber ; i++)
+        for (int i=0; i < glousNumber; i++)
         {
-            GameObject newGlou = Instantiate(glou,spawnTransform.position,Quaternion.identity);     // Spawn Glou
+            GameObject newGlou = Instantiate(m_glou, m_spawnTransform.position, Quaternion.identity); // Spawn Glou
 
             // Change Size
-            float scale = Random.Range(scaleMin, scaleMax);
-            newGlou.transform.localScale = new Vector3(scale,scale,scale);
+            float scale = Random.Range(m_scaleMin, m_scaleMax);
+            newGlou.transform.localScale = new Vector3(scale, scale, scale);
             
-            AudioManager.Instance.PlayGlouSpawn();  // Glou spawn sound
+            AudioManager.Instance.PlayGlouSpawn(); // Glou spawn sound
             GameManager.Instance.AddGlou();
 
-            float xRandomForce = Random.Range(-xMaxSpawnForce, xMaxSpawnForce);                     // Calculte force in X
-            newGlou.GetComponentInChildren<Rigidbody2D>().AddForce(new Vector2(xRandomForce,-ySpawnForce));   // Add force on the new glou
+            float xRandomForce = Random.Range(-m_xMaxSpawnForce, m_xMaxSpawnForce); // Calculte force in X
+            newGlou.GetComponentInChildren<Rigidbody2D>().AddForce(new Vector2(xRandomForce, -m_ySpawnForce)); // Add force on the new glou
 
-            yield return new WaitForSeconds(spawnDelay);
+            yield return new WaitForSeconds(m_spawnDelay);
         }
     }
 }
