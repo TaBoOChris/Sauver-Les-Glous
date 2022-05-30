f354332f8675f8a2fb95b5800ea875ff8da4a978
THIBAUD SIMON <tsimon1@etu.uqac.ca>
2022-05-30 16:22:09 -0400

    merge

--- Unity Sauver Les Glous/Assets/Scripts/Managers/VillageManager.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Managers/VillageManager.cs
@@ -8,6 +8,63 @@ public class VillageManager : AbstractSingleton<VillageManager>
     [SerializeField] private Canvas m_canvasBasket;
     [SerializeField] private GameObject m_GlouUIPrefab;
 
+    // Liste des Glous dans le village 
+    [Header("Spawn properties")]
+    private List<GameObject> m_glousInVillage = new List<GameObject>();
+    [SerializeField] private GameObject m_glouInVillagePrefab;
+
+    private void Start()
+    {
+        // temporaire
+        GlousData.Instance.AddGlouToVillage(new Glou(0.1f, 0.14f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.2f, 0.18f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.3f, 0.24f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.4f, 0.28f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.5f, 0.31f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.6f, 0.12f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.7f, 0.11f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.8f, 0.18f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.9f, 0.18f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(1, 0.18f), 0);
+
+        SpawnGlousInVillage();
+    }
+
+    private void SpawnGlousInVillage()
+    {
+        foreach (Glou glouData in GlousData.Instance.GetGlousInVillage())
+        {
+            // Spawn du glou
+            GameObject glou = Instantiate(m_glouInVillagePrefab, new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0), Quaternion.identity, transform);
+
+            // Get glou data
+            GlouInVillage data = glou.GetComponent<GlouInVillage>();
+            data.SetHue(glouData.hue);
+            data.SetHouseID(glouData.houseID);
+            data.SetSize(glouData.sizeMultiplier);
+
+            // Application des data au glou à faire spawn
+            SpriteRenderer glouBody = data.GetBodyRenderer();
+            SpriteRenderer glouExpression = data.GetExpressionRenderer();
+
+            // couleur
+            glouBody.color = Color.HSVToRGB(data.GetHue(), 1, 1);
+
+            // taille
+            float size = data.GetSize();
+            Vector3 scale = new Vector3(size, size, size);
+            glouBody.transform.localScale = scale;
+            glouExpression.transform.localScale = scale;
+
+            // maison
+            // to do
+
+
+            m_glousInVillage.Add(glou);
+        }
+
+    }
+
     public void DropGlou(GlouDragSelect glou)
     {
         // Check if within the dropzone
