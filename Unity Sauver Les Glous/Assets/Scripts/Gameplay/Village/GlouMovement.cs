48dc475784251be82d3ba84bfb457c0d14a719d5
Mathieu Dubois <mathieudubois403@gmail.com>
2022-05-30 18:52:30 -0400

    [dev] Les glous ne peuvent pas aller dans l'eau

--- Unity Sauver Les Glous/Assets/Scripts/Gameplay/Village/GlouMovement.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Gameplay/Village/GlouMovement.cs
@@ -7,7 +7,13 @@ public class GlouMovement : MonoBehaviour
     // Zone autour du Glou (un carré de "rayon" X parceque flemme de faire un cercle ça revient au même ^^)
     private int m_rayon = 3;
 
-    private float m_timeThreshold = 1f;
+    // Zone autorisé pour les déplacement
+    private float m_ilandMaxX = 12.57f;
+    private float m_ilandMinX = -19.39f;
+    private float m_ilandMaxY = 7.41f;
+    private float m_ilandMinY = -12.38f;
+
+    private float m_timeThreshold = 0.1f;
     private float m_nextMoveTimer;
 
     private Vector3 m_destination;
@@ -36,9 +42,13 @@ public class GlouMovement : MonoBehaviour
         Vector3 glouPos = transform.position;
 
         float maxX = glouPos.x + m_rayon;
+        if (maxX > m_ilandMaxX) maxX = m_ilandMaxX;
         float minX = glouPos.x - m_rayon;
+        if (minX < m_ilandMinX) minX = m_ilandMinX;
         float maxY = glouPos.y + m_rayon;
+        if (maxY > m_ilandMaxY) maxY = m_ilandMaxY;
         float minY = glouPos.y - m_rayon;
+        if (minY < m_ilandMinY) minY = m_ilandMinY;
 
         return new Vector3(Random.Range(minX, minY), Random.Range(minY, maxY), 0);
     }
