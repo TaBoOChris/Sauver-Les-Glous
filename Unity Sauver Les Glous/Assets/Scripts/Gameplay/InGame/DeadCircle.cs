58e96edf7608081477fd35d134f7ced8d673c587
Robin <robin.galmiche@yahoo.fr>
2022-05-30 11:20:07 -0400

    Add ghost when glou die

--- Unity Sauver Les Glous/Assets/Scripts/Gameplay/InGame/DeadCircle.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Gameplay/InGame/DeadCircle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DeadCircle : MonoBehaviour
 {
+    [SerializeField] private GameObject m_glouGhost;
+    [SerializeField] private GameObject m_glousGhostParent;
+
     // Start is called before the first frame update
     void OnTriggerExit2D(Collider2D other)
     {
@@ -15,9 +18,16 @@ public class DeadCircle : MonoBehaviour
         {
             GameManager.Instance.GlouDie();
             AudioManager.Instance.PlayGlouDie();
+            StartCoroutine(GlouDieAnimation_Coroutine(other));
         }
 
-
         Destroy(other.transform.parent.gameObject,0.2f);
+    }
+
+    IEnumerator GlouDieAnimation_Coroutine(Collider2D other)
+    {
+        GameObject newGlou = Instantiate(m_glouGhost, other.transform.position + new Vector3(0f, 0.5f, 0f), Quaternion.identity, m_glousGhostParent.transform);
+        yield return new WaitForSeconds(1.0f);
+        Destroy(newGlou);
     }
 }
