226eec1eb150a6c163b7b2c00469fb20d94a8adb
Robin <robin.galmiche@yahoo.fr>
2022-05-30 19:25:58 -0400

    Add Speed up and down + reverse to game

--- Unity Sauver Les Glous/Assets/Scripts/Managers/LevelProperties.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Managers/LevelProperties.cs
@@ -6,20 +6,49 @@ public class LevelProperties : AbstractSingleton<LevelProperties>
 {
     [Header("Level properties")]
     public float rotationSpeed = 10f;
+    [SerializeField] private float m_maxRotationSpeed = 2f;
+    [SerializeField] private float m_minRotationSpeed = 0.7f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    private bool m_isSpeedUp = false;
 
-    // Update is called once per frame
-    void Update()
+    public void SpeedUpDown()
     {
-        
+        if (!m_isSpeedUp)
+        {
+            if(rotationSpeed <= m_minRotationSpeed && rotationSpeed >= 0)
+            {
+                rotationSpeed = -m_minRotationSpeed;
+            }
+            else
+            {
+                rotationSpeed -= 0.2f;
+            }
+            if (rotationSpeed <= -m_maxRotationSpeed)
+            {
+                m_isSpeedUp = !m_isSpeedUp;
+            }
+        }
+    else
+        {
+            if (rotationSpeed >= -m_minRotationSpeed && rotationSpeed <= 0)
+            {
+                rotationSpeed = m_minRotationSpeed;
+            }
+            else
+            {
+                rotationSpeed += 0.2f;
+            }
+            if (rotationSpeed >= m_maxRotationSpeed)
+            {
+                m_isSpeedUp = !m_isSpeedUp;
+            }
+        }
+
     }
 
     public void SetRotationSpeed(float newRotationSpeed)
     {
         rotationSpeed = newRotationSpeed;
     }
+
 }
