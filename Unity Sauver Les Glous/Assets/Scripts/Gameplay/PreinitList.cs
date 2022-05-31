83230cb18da782dee76ec260c58a7af8dbdb8775
THIBAUD SIMON <tsimon1@etu.uqac.ca>
2022-05-30 23:39:13 -0400

    village glous to selector

--- Unity Sauver Les Glous/Assets/Scripts/Gameplay/PreinitList.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Gameplay/PreinitList.cs
@@ -10,5 +10,18 @@ public class PreinitList : MonoBehaviour
     {
         GlousData.Instance.SetGlousInSelector(m_glousStartingList);
         Debug.Log("Glous in selector : " + GlousData.Instance.GetGlousInSelector().Count);
+
+        GlousData.Instance.AddGlouToVillage(new Glou(0.1f, 0.14f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.2f, 0.18f), 1);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.3f, 0.24f), 1);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.4f, 0.28f), 2);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.5f, 0.31f), 3);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.6f, 0.12f), 4);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.7f, 0.11f), 4);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.8f, 0.18f), 5);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.9f, 0.18f), 3);
+        GlousData.Instance.AddGlouToVillage(new Glou(1, 0.18f), 0);
+        Debug.Log("Glous in selector : " + GlousData.Instance.GetGlousInVillage().Count);
+
     }
 }
--- Unity Sauver Les Glous/Assets/Scripts/Managers/VillageManager.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Managers/VillageManager.cs
@@ -35,18 +35,6 @@ public class VillageManager : AbstractSingleton<VillageManager>
         }
         Debug.Log("Village registered " + m_villageHouses.Count +" houses.");
 
-        // temporaire
-        GlousData.Instance.AddGlouToVillage(new Glou(0.1f, 0.14f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.2f, 0.18f), 1);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.3f, 0.24f), 1);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.4f, 0.28f), 2);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.5f, 0.31f), 3);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.6f, 0.12f), 4);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.7f, 0.11f), 4);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.8f, 0.18f), 5);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.9f, 0.18f), 3);
-        GlousData.Instance.AddGlouToVillage(new Glou(1, 0.18f), 0);
-
         SpawnGlousInVillage();
     }
 
@@ -113,7 +101,7 @@ public class VillageManager : AbstractSingleton<VillageManager>
                 Destroy(glou.gameObject);
 
                 //Move to selector
-                //GlousData.Instance.MoveGlouToSelector(ref data);
+                GlousData.Instance.MoveGlouToSelector(ref data);
 
                 //UI Show in basket
                 GameObject glouUI = Instantiate(m_GlouUIPrefab, m_canvasBasket.transform, true);
