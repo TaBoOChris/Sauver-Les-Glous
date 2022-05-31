a9f06f127d9be12312fb77fcc1eeb46b3d47bf4d
THIBAUD SIMON <tsimon1@etu.uqac.ca>
2022-05-30 20:22:52 -0400

    Glous spawn in houses with 40% chance

--- Unity Sauver Les Glous/Assets/Scripts/Managers/VillageManager.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Managers/VillageManager.cs
@@ -31,14 +31,14 @@ public class VillageManager : AbstractSingleton<VillageManager>
 
         // temporaire
         GlousData.Instance.AddGlouToVillage(new Glou(0.1f, 0.14f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.2f, 0.18f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.3f, 0.24f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.4f, 0.28f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.5f, 0.31f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.6f, 0.12f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.7f, 0.11f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.8f, 0.18f), 0);
-        GlousData.Instance.AddGlouToVillage(new Glou(0.9f, 0.18f), 0);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.2f, 0.18f), 1);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.3f, 0.24f), 1);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.4f, 0.28f), 2);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.5f, 0.31f), 3);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.6f, 0.12f), 4);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.7f, 0.11f), 4);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.8f, 0.18f), 5);
+        GlousData.Instance.AddGlouToVillage(new Glou(0.9f, 0.18f), 3);
         GlousData.Instance.AddGlouToVillage(new Glou(1, 0.18f), 0);
 
         SpawnGlousInVillage();
@@ -49,7 +49,7 @@ public class VillageManager : AbstractSingleton<VillageManager>
         foreach (Glou glouData in GlousData.Instance.GetGlousInVillage())
         {
             // Spawn du glou
-            GameObject glou = Instantiate(m_glouInVillagePrefab, new Vector3(Random.Range(-19.39f, 12.57f), Random.Range(-12.38f, 7.41f), 0), Quaternion.identity, transform);
+            GameObject glou = Instantiate(m_glouInVillagePrefab, transform.position, Quaternion.identity, transform);
 
             // Get glou data
             GlouInVillage data = glou.GetComponent<GlouInVillage>();
@@ -71,9 +71,16 @@ public class VillageManager : AbstractSingleton<VillageManager>
             glouBody.transform.localScale = scale;
             glouExpression.transform.localScale = scale;
 
-            // maison
-            // to do
-
+            // maison -- 60% de chance de spawn dedans
+            if (Random.Range(0f,1.0f) >= 0.4f)
+            {
+                House glouHouse = m_villageHouses.Find(house => house.GetHouseID() == glouData.houseID);
+                glou.transform.position = PositionInHouse(glouHouse.GetComponent<BoxCollider2D>().bounds);
+            }
+            else
+            {
+                glou.transform.position = new Vector3(Random.Range(-19.39f, 12.57f), Random.Range(-12.38f, 7.41f), 0);
+            }
 
             m_glousInVillage.Add(glou);
         }
@@ -118,10 +125,7 @@ public class VillageManager : AbstractSingleton<VillageManager>
                     Vector3 target = glou.transform.position;
                     if (m_SetPositionInHouse)
                     {
-                        target = new Vector3(
-                            Random.Range(b.min.x + 0.2f, b.max.x - 0.2f),
-                            Random.Range(b.min.y + 0.2f, b.max.y - 0.2f),
-                            0  );
+                        target = PositionInHouse(b);
                     }
 
                     glou.transform.position = b.ClosestPoint(target);
@@ -154,5 +158,13 @@ public class VillageManager : AbstractSingleton<VillageManager>
 
     }
 
+
+    static Vector3 PositionInHouse(Bounds b)
+    {
+        return new Vector3(
+            Random.Range(b.min.x + 0.5f, b.max.x - 0.5f),
+            Random.Range(b.min.y + 0.75f, b.max.y - 0.75f),
+            0);
+    }
 
 }
