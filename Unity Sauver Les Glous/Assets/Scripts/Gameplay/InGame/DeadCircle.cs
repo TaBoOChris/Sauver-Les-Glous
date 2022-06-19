3751c81bc6c513ddab04309b811b77b1fa4f6959
TaBoO Chris <florent.bogacz@gmail.com>
2022-06-19 16:09:06 -0400

    Deplacement de la gestion de la mort d'un glou in game

--- Unity Sauver Les Glous/Assets/Scripts/Gameplay/InGame/DeadCircle.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Gameplay/InGame/DeadCircle.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class DeadCircle : MonoBehaviour
 {
-    [SerializeField] private GameObject m_glouGhost;
-    [SerializeField] private GameObject m_glousGhostParent;
+
 
     // Start is called before the first frame update
     void OnTriggerExit2D(Collider2D other)
@@ -16,19 +15,8 @@ public class DeadCircle : MonoBehaviour
         Debug.Log("Kill Glou");
         other.gameObject.GetComponent<GlouInGame>().KillGlou();
 
-        // effects of death
-        GameManager.Instance.GlouDie();
-        AudioManager.Instance.PlayGlouDie();
-        StartCoroutine(GlouDieAnimation_Coroutine(other));
 
-        Destroy(other.gameObject);
     }
 
-    IEnumerator GlouDieAnimation_Coroutine(Collider2D other)
-    {
-        GameObject newGlou = Instantiate(m_glouGhost, other.transform.position + new Vector3(0f, 0.5f, 0f), Quaternion.identity, m_glousGhostParent.transform);
-        newGlou.transform.localScale = other.transform.localScale;
-        yield return new WaitForSecondsRealtime(1.0f);
-        Destroy(newGlou);
-    }
+
 }
--- Unity Sauver Les Glous/Assets/Scripts/Gameplay/InGame/GlouInGame.cs
+++ Unity Sauver Les Glous/Assets/Scripts/Gameplay/InGame/GlouInGame.cs
@@ -7,6 +7,8 @@ public class GlouInGame : MonoBehaviour
     private Glou m_glou = null;
     private bool m_isAlive = true;
 
+    [SerializeField] private GameObject m_glouGhost;
+
     public void SetGlou(Glou glou)
     {
         m_glou = glou;
@@ -20,10 +22,31 @@ public class GlouInGame : MonoBehaviour
     public void KillGlou()
     {
         m_isAlive = false;
+
+        // effects of death
+        GameManager.Instance.GlouDie();
+        AudioManager.Instance.PlayGlouDie();
+        StartCoroutine(GlouDieAnimation_Coroutine());
+
+        Destroy(gameObject);
     }
 
     public bool IsAlive()
     {
         return m_isAlive;
+    }
+
+
+    IEnumerator GlouDieAnimation_Coroutine()
+    {
+        GameObject newGlou = Instantiate(
+            m_glouGhost, 
+            transform.position + new Vector3(0f, 0.5f, 0f), 
+            Quaternion.identity
+            );
+
+        newGlou.transform.localScale = transform.localScale;
+        yield return new WaitForSecondsRealtime(1.0f);
+        Destroy(newGlou);
     }
 }
